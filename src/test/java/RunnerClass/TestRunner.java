28599ffbc1ec2dd95f77edf6238c8f63aec607de
hari <harivom577@gmail.com>
2022-06-13 09:55:26 +0530

    firstone

--- src/test/java/RunnerClass/TestRunner.java
+++ src/test/java/RunnerClass/TestRunner.java
@@ -5,9 +5,9 @@ import io.cucumber.junit.CucumberOptions;
 import io.cucumber.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features/BackgroundDemo/Registpage.feature", 
+@CucumberOptions(features="src/test/resources/Features/BackgroundDemo/Register_opencart.feature", 
 glue= {"StepDefinitions"},
-monochrome= true,strict=true,dryRun=true,
+monochrome= true,strict=false,dryRun=false,
 plugin = {"pretty", "junit:target/JUnitReports/report.xml",
 		"json:target/JSONReports/report.json",
 		"html:target/HtmlReports"}
